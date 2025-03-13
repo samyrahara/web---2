b801ae908af55fc144878d66b695a2a0eb5d9cdd
samyrahara <marisaazzahra01@gmail.com>
2025-03-13 10:39:42 +0700

    praktikum4

---
+++ praktikum04/class_animal.php
@@ -0,0 +1,46 @@
+<?php
+
+class Animal {
+    public $animals;
+
+    
+    public function __construct($ar_animal) {
+        $this->animals = $ar_animal;
+    }
+
+    
+    public function index() {
+        foreach ($this->animals as $animal) {
+            echo "- $animal <br/>";
+        }
+    }
+
+    
+    public function store($animal) {
+        $this->animals[] = $animal;
+    }
+
+    public function update($index, $animal)
+    {
+        $this->animals[$index] = $animal;
+    }
+
+    public function destroy(){}
+}
+
+
+$animal = new Animal(["Ayam", "Ikan"]);
+
+echo "Index - Menampilkan seluruh hewan <br/>";
+$animal->index();
+echo "<br/>";
+
+echo "Store - Menambahkan hewan baru (Burung) <br/>";
+$animal->store("Burung"); 
+$animal->index();
+echo "<br/>";
+
+echo "Update - Mengupdate hewan <br/>";
+$animal->store(0, "Kucing Angora"); 
+$animal->index();
+echo "<br/>";
