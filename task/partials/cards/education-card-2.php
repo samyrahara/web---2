bb9432fbbc18361ac5c08d24f543716bd4eb2a31
samyrahara <marisaazzahra01@gmail.com>
2025-03-13 12:03:42 +0700

    praktikum3

---
+++ task/partials/cards/education-card-2.php
@@ -0,0 +1,19 @@
+<div class="card shadow border-0 rounded-4 mb-5">
+                                <div class="card-body p-5">
+                                    <div class="row align-items-center gx-5">
+                                        <div class="col text-center text-lg-start mb-4 mb-lg-0">
+                                            <div class="bg-light p-4 rounded-4">
+                                                <div class="text-secondary fw-bolder mb-2">2021 - 2024</div>
+                                                <div class="mb-2">
+                                                    <div class="small fw-bolder">SMAN 1 Rancabungur</div>
+                                                    <div class="small text-muted">JL LETKOL ATANG SANJAYA, Pasir Gaok, Kec. Ranca Bungur, Kab. Bogor Prov. Jawa Barat </div>
+                                                </div>
+                                                <div class="fst-italic">
+                                                    <div class="small text-muted">Siswa</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-8"><div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus laudantium, voluptatem quis repellendus eaque sit animi illo ipsam amet officiis corporis sed aliquam non voluptate corrupti excepturi maxime porro fuga.</div></div>
+                                    </div>
+                                </div>
+                            </div>
