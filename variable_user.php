924010e568802a131a727eb8d8d8af940b5a371d
samyrahara <marisaazzahra01@gmail.com>
2025-03-13 10:15:23 +0700

    Menyimpan perubahan di praktikum3 sebelum menghapus dari tracking

---
+++ variable_user.php
@@ -0,0 +1,12 @@
+<?php
+// mendefinisikan variable
+
+$nama = 'Marissa';
+$umur = 18;
+$berat = 50;
+
+echo 'Nama saya ' . $nama;
+echo '<br/>Umur : ' . $umur . 'Tahun';
+echo '<br/>Berat : ' . $berat .'Kg';
+
+?>
